cc36c264050b6dfd146f8d7e0b6910733a0a3f64
jitehikaru <1362155791@qq.com>
2018-06-25 13:55:36 +0800

    ss

---
+++ chapter1/udp_client.c
@@ -0,0 +1,34 @@
+#include    "cliserv.h"
+
+int
+main(int argc, char **argv)
+{
+    struct sockaddr_in serv;
+    char   request[REQUEST], reply[REPLY];
+    int    sockfd, n;
+
+    if (argc !=2)
+        err_quit("usage: udpcli <IP address of server>");
+
+    if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+        err_sys("socket error");
+
+    memset(&serv, 0, sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = inet_addr(argv[1]);
+    serv.sin_port = htons(UDP_SERV_PORT);
+
+    /* from request[] ... */
+
+    if (sendto(sockfd, request, REQUEST, 0,
+               (SA) &serv, sizeof(serv)) != REQUEST)
+        err_sys("sendto error");
+
+    if ((n = recvfrom(sockfd, reply, REPLY, 0,
+                     (SA) NULL, (int *) NULL)) < 0)
+        err_sys("recvfrom error");
+
+    /* process "n" bytes of reply[] ... */
+
+    exit(0);
+}
